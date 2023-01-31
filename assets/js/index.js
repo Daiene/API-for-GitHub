2788ac61d21f4b491557b27f86b752e7787b5cd3
Daiene Batagioti <daiene.montuani@hotmail.com>
2023-01-31 15:35:27 -0300

    Trying to fix repository button and location image

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ const user = document.querySelector(".user");
 const button = document.querySelector(".button");
 const modal = document.querySelector("dialog");
 const modalClose = document.querySelector(".modal-close");
+const buttonRepository = document.querySelector(".button-repository");
 
 button.onclick = function () {
     modal.showModal();
@@ -16,28 +17,30 @@ button.addEventListener("click", () => {
         .then(function (responseJson) {
             modal.insertAdjacentHTML("beforeend", `
         <div class="informacoes">
-            <img src="${responseJson.avatar_url}" alt="" />
+            <img class="perfil" src="${responseJson.avatar_url}" alt="" />
             <h1>${responseJson.name}</h1>
-            <p>${responseJson.location}</p>
+            <p><img src="/images/localizacao/logo-local.jpg">${responseJson.location}</p>
             <p>${responseJson.bio}</p>
         </div>
     `)
         })
 
-    fetch(`https://api.github.com/users/${inputValue}/repos`)
-        .then(function (response) { return response.json(); })
-        .then(function (responseJson) {
-            responseJson.map(repository => modal.insertAdjacentHTML("beforeend", `
-        <div class="repositorio">
-            <h2>${repository.name}</h2>
-            <a 
-            href="${repository.html_url}"     
-            target="_blank">
-            Ver código
-            </a>
-        </div>`
-            ))
-        })
+    buttonRepository.addEventListener("click", () => {
+        fetch(`https://api.github.com/users/${inputValue}/repos`)
+            .then(function (response) { return response.json(); })
+            .then(function (responseJson) {
+                responseJson.map(repository => modal.insertAdjacentHTML("beforeend", `
+                    <div class="repositorio">
+                        <h2>${repository.name}</h2>
+                        <a 
+                        href="${repository.html_url}"     
+                        target="_blank">
+                        Ver código
+                        </a>
+                    </div>`
+                ))
+            })
+    })
 
     modal.addEventListener('click', event => {
         const nomeDaClasseDoElementoClicado = event.target.classList[0];
